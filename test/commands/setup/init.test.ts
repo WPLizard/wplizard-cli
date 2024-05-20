3b5e81d8b5911fe85b154e20aa713b9b9ae2378f
OVECJOE <ovecjoe123@gmail.com>
2024-05-20 07:57:10 +0100

    test: added boring unit tests for the setup:init command

--- test/commands/setup/init.test.ts
+++ test/commands/setup/init.test.ts
@@ -1,17 +1,63 @@
-import {expect, test} from '@oclif/test'
+import {Config, expect, test} from '@oclif/test'
+
+import Init from '../../../src/commands/setup/init.js'
+
+class InitTest extends Init {
+  public async parseFlags(argv: string[]) {
+    return this.parse(Init, argv)
+  }
+}
 
 describe('setup:init', () => {
+  // test that the command exists
   test
-  .stdout()
-  .command(['setup:init'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+    .stdout()
+    .command(['setup:init'])
+    .it('command exists', ctx => {
+      expect(ctx.stdout).to.contain('setup wizard');
+    })
 
   test
-  .stdout()
-  .command(['setup:init', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['setup:init'])
+    .it('All flags should be set to false by default', () => {
+      const cmd = new InitTest([], new Config({
+        name: 'wplizard-cli',
+        root: process.cwd(),
+      }))
+
+      cmd.parseFlags([]).then(({ flags }) => {
+        expect(flags.lazy).to.be.false
+        expect(flags.skeleton).to.be.false
+        expect(flags.runInstallers).to.be.false
+      })
+    })
+
+  test
+    .stdout()
+    .command(['setup:init', '--lazy'])
+    .it('Every flag should be set to true when passed', () => {
+      const cmd = new InitTest([], new Config({
+        name: 'wplizard-cli',
+        root: process.cwd(),
+      }))
+
+      cmd.parseFlags(['--lazy']).then(({ flags }) => {
+        expect(flags.lazy).to.be.true
+        expect(flags.skeleton).to.be.false
+        expect(flags.runInstallers).to.be.false
+      })
+
+      cmd.parseFlags(['--skeleton']).then(({ flags }) => {
+        expect(flags.lazy).to.be.false
+        expect(flags.skeleton).to.be.true
+        expect(flags.runInstallers).to.be.false
+      })
+
+      cmd.parseFlags(['--run-installers']).then(({ flags }) => {
+        expect(flags.lazy).to.be.false
+        expect(flags.skeleton).to.be.false
+        expect(flags.runInstallers).to.be.true
+      })
+    })
 })
